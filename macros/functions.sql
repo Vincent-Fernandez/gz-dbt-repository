21da54304b3c3c6ea3203b953c47fc3698da2fd4
Vincent Fernandez <vfz66lewagon@gmail.com>
2024-05-08 10:05:49 +0000

    function to compute % margin

---
+++ macros/functions.sql
@@ -0,0 +1,11 @@
+{% macro margin_percent(revenue, purchase_cost) %}
+
+ROUND(
+    SAFE_DIVIDE(
+        ({{ revenue }} - {{ purchase_cost }}), 
+        {{ revenue }}
+    ),
+2
+)
+
+{% endtest %}
